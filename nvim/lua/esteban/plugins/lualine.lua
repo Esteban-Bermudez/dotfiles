3767c48874d9819bf0e5c50fa18f32fbe2d565c9
Esteban-Bermudez <esteban@super.gd>
2024-05-12 00:12:56 -0700

    Remove the branch name from lualine
    This took up way too much space and it is already inclued in zsh PS1

--- nvim/lua/esteban/plugins/lualine.lua
+++ nvim/lua/esteban/plugins/lualine.lua
@@ -7,7 +7,8 @@ require('lualine').setup({
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'diff', 'branch' },
+    -- lualine_b = { 'diff', 'branch' },
+    lualine_b = { 'diff', },
     lualine_c = { 'b:gitsigns_blame_line' },
     lualine_x = { 'diagnostics' },
     lualine_y = { { 'filename', path = 1, symbols = { modified = '●' }, }, { 'filetype', padding = 0, icon_only = true }, },
