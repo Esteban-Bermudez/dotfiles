f46ad93c3adffca12f779af9175471ff589cf3ab
Esteban-Bermudez <esteban@bermudezaguirre.com>
2024-12-14 21:44:34 -0800

    Clean up lualine configuration
    Removed the git blame line and added the branch name back to the status line.

--- nvim/lua/esteban/plugins/lualine.lua
+++ nvim/lua/esteban/plugins/lualine.lua
@@ -4,15 +4,17 @@ require('lualine').setup({
     section_separators = '',
     component_separators = '',
     globalstatus = true,
-    ignore_focus = {}
+    ignore_focus = { 'NvimTree', 'packer', 'TelescopePrompt' },
+    disabled_filetypes = {
+      statusline = { 'NvimTree', 'packer' },
+      tabline = { 'NvimTree', 'packer' }
+    }
   },
   sections = {
     lualine_a = { 'mode' },
-    -- lualine_b = { 'diff', 'branch' },
-    lualine_b = { 'diff' },
-    -- lualine_c = { 'b:gitsigns_blame_line' },
+    lualine_b = { 'diff', 'branch' },
     lualine_c = { { 'filename', path = 1, file_status = false, symbols = { modified = '●', unnamed = '~' }, }, },
-    lualine_x = { { 'filetype', icon_only = true, align = 'right' }, },
+    lualine_x = { { 'filetype', icon_only = false, align = 'right' }, },
     lualine_y = { 'diagnostics' },
     lualine_z = { 'location' },
   },
@@ -23,10 +25,15 @@ require('lualine').setup({
     lualine_c = { {
       'buffers',
       show_filename_only = true,
+      icons_enabled = false,
       show_modified_status = true,
       symbols = {
         alternate_file = '',
-      }
+      },
+      filetype_names = {
+        TelescopePrompt = '',
+        NvimTree = '',
+      },
     } },
     lualine_x = {},
     lualine_y = {},
