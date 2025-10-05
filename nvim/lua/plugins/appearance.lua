b326edb664af6e6492a0e699384165930168003c
Esteban-Bermudez <esteban@bermudezaguirre.com>
2025-10-04 23:04:51 -0700

    Fix neovim structure and support Omarchy themes
    This makes it so that my neovim config will work better with the setup
    that Omarchy is used to. It also restructures the files a bit more into
    use case categories and I will still need to clean up a few for better
    file specific and LSP support.
    
    The main change is to colour theme which will check the omarchy theme
    and match it if we are not on macOS.

---
+++ nvim/lua/plugins/appearance.lua
@@ -0,0 +1,91 @@
+vim.opt.showmode = false
+vim.opt.cmdheight = 0
+vim.api.nvim_create_autocmd("RecordingEnter", {
+	callback = function()
+		vim.opt.cmdheight = 1
+	end,
+})
+vim.api.nvim_create_autocmd("RecordingLeave", {
+	callback = function()
+		vim.opt.cmdheight = 0
+	end,
+})
+
+return {
+	-- Hex Colours
+	{
+		"catgoose/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+
+  -- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate", -- Automatically update parsers on plugin build
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- Configure treesitter modules
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = true,
+				},
+				indent = { enable = true },
+				folds = { enable = true },
+			})
+			vim.treesitter.language.register("markdown", "mdx")
+		end,
+	},
+
+  -- Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        theme = "auto",
+        section_separators = "",
+        component_separators = "",
+        globalstatus = true,
+        ignore_focus = { "NvimTree", "packer", "TelescopePrompt" },
+        disabled_filetypes = {
+          statusline = { "NvimTree", "packer" },
+          tabline = { "NvimTree", "packer" },
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "diff", "branch" },
+        lualine_c = { { "filename", path = 1, file_status = false, symbols = { modified = "●", unnamed = "~" } } },
+        lualine_x = { { "filetype", icon_only = false, align = "right" } },
+        lualine_y = { "diagnostics" },
+        lualine_z = { "location" },
+      },
+      tabline = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "buffers",
+            show_filename_only = true,
+            icons_enabled = false,
+            show_modified_status = true,
+            symbols = {
+              alternate_file = "",
+            },
+            filetype_names = {
+              TelescopePrompt = "",
+              NvimTree = "",
+            },
+          },
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
+}
