edace644ac283bf28668fdc0573835a767a823e8
Esteban-Bermudez <esteban@bermudezaguirre.com>
2025-11-07 11:40:33 -0800

    Use mini.pairs over autoclose
    Mini pairs seems to be better than what I have looked into autoclose. It
    seems to work better when working with strings and markdown. I took this
    config from LazyVim.

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,17 +3,27 @@ return {
 	"tpope/vim-commentary",
 	"tpope/vim-surround",
 
-  -- Tmux integration
+	-- Tmux integration
 	"christoomey/vim-tmux-navigator",
 
 	-- Github Copilot
 	"github/copilot.vim",
 
+  -- Auto Bracket and Quote Pairing
 	{
-		"m4xshen/autoclose.nvim",
-		config = function()
-			require("autoclose").setup()
-		end,
+		"nvim-mini/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
 	},
-
 }
