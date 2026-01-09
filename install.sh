10863868d9770f504855ccf6219dd59ddc919de8
Esteban-Bermudez <esteban@bermudezaguirre.com>
2026-01-09 13:36:10 -0800

    Add installation of GUI apps to script

--- install.sh
+++ install.sh
@@ -60,6 +60,57 @@ install_fonts() {
   rm /tmp/FiraMono.zip
 }
 
+install_docker() {
+  if ! command -v docker &> /dev/null; then
+    echo "Installing Docker..."
+    curl -L https://desktop.docker.com/mac/main/arm64/Docker.dmg -o /tmp/Docker.dmg
+    hdiutil attach /tmp/Docker.dmg
+    cp -R /Volumes/Docker/Docker.app /Applications
+    hdiutil detach /Volumes/Docker
+    rm /tmp/Docker.dmg
+    echo ""
+    echo " * Docker is now installed. Please open Docker.app from Applications to finish setup. * "
+    echo ""
+  else
+    echo "Docker is already installed. Skipping installation."
+  fi
+}
+
+install_scroll_reverser() {
+  if ! [ -d "/Applications/Scroll Reverser.app" ]; then
+    echo "Installing Scroll Reverser..."
+    # Zip contains the Scroll Reverser app
+    curl -L https://pilotmoon.com/downloads/ScrollReverser-1.9.zip -o /tmp/ScrollReverser.zip
+    unzip -o /tmp/ScrollReverser.zip -d /Applications
+    rm /tmp/ScrollReverser.zip
+    echo ""
+    echo " * Scroll Reverser is now installed. Please open Scroll Reverser from Applications to finish setup. * "
+    echo ""
+  else
+    echo "Scroll Reverser is already installed. Skipping installation."
+  fi
+}
+
+install_rectangle() {
+  if ! [ -d "/Applications/Rectangle.app" ]; then
+    echo "Installing Rectangle..."
+    curl -L https://github.com/rxhanson/Rectangle/releases/download/v0.93/Rectangle0.93.dmg -o /tmp/Rectangle0.93.dmg
+    MOUNT_OUTPUT=$(hdiutil attach /tmp/Rectangle0.93.dmg)
+    MOUNT_POINT=$(echo "$MOUNT_OUTPUT" | grep "/Volumes/" | awk 'BEGIN{FS="	"}{print $3}')
+    cp -R "${MOUNT_POINT}/Rectangle.app" /Applications
+    hdiutil detach "${MOUNT_POINT}"
+    rm /tmp/Rectangle0.93.dmg
+    # Remove the quarantine attribute to prevent Gatekeeper issues
+    xattr -d com.apple.quarantine "/Applications/Rectangle.app"
+    echo ""
+    echo " * Rectangle is now installed. Please open Rectangle from Applications to finish setup. * "
+    echo ""
+  else
+    echo "Rectangle is already installed. Skipping installation."
+  fi
+}
+
+
 # Function to install Kitty terminal emulator
 install_kitty() {
   #curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
@@ -84,9 +135,9 @@ main() {
   echo "Installing fonts..."
   install_fonts
   echo "Installing GUI applications..."
-  # install_docker
-  # install_scroll_reverser
-  # install_rectangle
+  install_docker
+  install_scroll_reverser
+  install_rectangle
   install_kitty
   echo "------------------------------------------------------------------------"
   echo "All installations are complete."
