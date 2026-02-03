c23d0d7132715b9a57f8c0fd4debde41bbf3081a
Esteban-Bermudez <esteban.bermudez@paved.com>
2026-02-02 23:59:44 -0800

    Set prettier formatter to use local config file

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -81,19 +81,9 @@ return {
 			-- The options you set here will be merged with the builtin formatters.
 			-- You can also define any custom formatters here.
 			formatters = {
-
-				injected = { options = { ignore_errors = true } },
-				-- # Example of using dprint only when a dprint.json file is present
-				-- dprint = {
-				--   condition = function(ctx)
-				--     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-				--   end,
-				-- },
-				--
-				-- # Example of using shfmt with extra args
-				-- shfmt = {
-				--   prepend_args = { "-i", "2", "-ci" },
-				-- },
+         prettier = {
+            require_cwd = true,
+        },
 			},
 		},
 	},
