d08c6b6bdc8c1781852e6d9a077d3bb4576fd7d9
Esteban-Bermudez <esteban.bermudez@paved.com>
2026-04-10 15:06:52 -0700

    Add markdown support
    This adds better highlight, preview and formatting to markdown files.
    Tis the age of AI were we only write MD files

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -57,6 +57,33 @@ return {
     end,
   },
 
+  -- Markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    opts = {},
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>mp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+
   -- Lualine
   {
     "nvim-lualine/lualine.nvim",
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,8 +14,10 @@ return {
       },
     },
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
+      {
+        "mason-org/mason.nvim",
+        opts = { ensure_installed = { "markdownlint-cli2", "markdown-toc" } },
+      },
     },
   },
 
@@ -70,19 +72,38 @@ return {
     opts = {
       default_format_opts = {
         timeout_ms = 3000,
-        async = false,       -- not recommended to change
-        quiet = false,       -- not recommended to change
+        async = false,           -- not recommended to change
+        quiet = false,           -- not recommended to change
         lsp_format = "fallback", -- not recommended to change
       },
       formatters_by_ft = {
         lua = { "stylua" },
         go = { "goimports" },
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
       },
       -- The options you set here will be merged with the builtin formatters.
       -- You can also define any custom formatters here.
       formatters = {
         prettier = {
           require_cwd = true,
+        },
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
         },
       },
     },
