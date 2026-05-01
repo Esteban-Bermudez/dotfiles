d55879d6ab32fb97859a06cee8c2455d3f7bf750
Esteban-Bermudez <esteban.bermudez@paved.com>
2026-05-01 15:49:48 -0700

    Make markdown rendering happen on keybind
    I did not like having this on by default but I like it when I am viewing
    files and also switching between.

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -61,7 +61,12 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    opts = {},
+    config = function()
+      require("render-markdown").setup({
+        enabled = false, -- Disable by default, toggle with keymap
+      })
+      vim.api.nvim_set_keymap('n', '<leader>mt', '<cmd>RenderMarkdown toggle<cr>', { noremap = true, silent = true })
+    end,
   },
 
   {
